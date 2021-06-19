ac9baa987d75fddf0b18f9be464f846161453a23
rald <thecoconutnutisagiantnut@gmail.com>
2021-06-19 11:06:52 +0800

    OK

--- TextTwist/src/edu/hogwarts/siesta/texttwist/TextTwist.java
+++ TextTwist/src/edu/hogwarts/siesta/texttwist/TextTwist.java
@@ -3,6 +3,8 @@ package edu.hogwarts.siesta.texttwist;
 import android.app.Activity;
 import android.os.Bundle;
 
+import android.media.AudioManager;
+
 public class TextTwist extends Activity {
 
   Animation animation;
@@ -10,6 +12,9 @@ public class TextTwist extends Activity {
   @Override
   public void onCreate(Bundle bundle) {
     super.onCreate(bundle);
+
+	setVolumeControlStream(AudioManager.STREAM_MUSIC);
+
     animation=new Animation(this);
     setContentView(animation);
   }
